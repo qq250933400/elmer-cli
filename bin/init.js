40fafee3c8e13bbd04630024d51be137d99900be
莫书江 <moshujiang@moshujiangdeMacBook-Pro.local>
2020-01-16 23:51:52 +0800

    add template configuration

--- bin/init.js
+++ bin/init.js
@@ -7,15 +7,20 @@ const supportProjects = [
         code: "A",
         message: "创建UI开发项目",
         sourcePath: "./ui",
+        exec: apiBuilder,
+        ignore: [/\/\.DS_Store$/, /\/api\/package\.json$/]
     }, {
         code: "B",
         message: "创建API开发项目",
         sourcePath: "./api",
-        exec: apiBuilder
+        exec: apiBuilder,
+        ignore: [/\/\.DS_Store$/, /\/api\/package\.json$/]
     },{
         code: "C",
         message: "创建服务端渲染项目",
         sourcePath: "./rsv",
+        exec: apiBuilder,
+        ignore: [/\/\.DS_Store$/, /\/api\/package\.json$/]
     }, {
         code: "Q",
         message: "退出程序"
@@ -24,21 +29,14 @@ const supportProjects = [
 const inputCode = (codeList, fn, tipMsg) => {
     inquirer.prompt([
         {
-            type: "input",
+            type: "list",
             name: "initCode",
-            message: tipMsg || "请输入要创建的项目编号 >>> "
+            message: tipMsg || "请选择创建项目模版: ",
+            choices: codeList
         }
     ]).then((resp) => {
         const code = (resp.initCode || "").toUpperCase();
-        if(codeList.indexOf(code)<0) {
-            inputCode(codeList, fn, color.red("[ERR] 输入编号不正确，请重新输入 >>> "));
-        } else {
-            if(code === "Q") {
-                console.log(color.yellow("[INFO] 结束创建流程。"));
-            } else {
-                typeof fn === "function" && fn(code);
-            }
-        }
+        typeof fn === "function" && fn(code);
     });
 };
 const inputProjectName = (fn, msg) => {
@@ -57,34 +55,42 @@ const inputProjectName = (fn, msg) => {
         }
     });
 };
+
 program.version("1.0.0")
+    .option("-y, --yarn", "指定yarn为包管理器")
     .command("init")
     .description("创建项目")
     .action(() => {
         const codeList = [];
-        console.log(color.blue.bold("[Init] 选择要创建的项目类型 (不区分大小写)"));
         supportProjects.map((item) => {
-            codeList.push(item.code.toUpperCase());
-            console.log(color.blue(`    [${item.code}] ${item.message}`));
+            codeList.push({
+                value: item.code.toUpperCase(),
+                name: "  [" + item.code.toUpperCase() + "] " + item.message
+            });
         });
         inputCode(codeList, (code) => {
             let project;
-            for(let i=0;i<supportProjects.length;i++) {
-                if(code === supportProjects[i].code.toUpperCase()) {
-                    project = supportProjects[i];
-                    break;
+            if(code !== "Q") {
+                for(let i=0;i<supportProjects.length;i++) {
+                    if(code === supportProjects[i].code.toUpperCase()) {
+                        project = supportProjects[i];
+                        break;
+                    }
                 }
+                inputProjectName((name) => {
+                    const exec = project.exec;
+                    delete project.exec;
+                    project.name = name;
+                    project.yarn = program.yarn;
+                    if(typeof exec === "function") {
+                        exec(project);
+                    } else {
+                        console.log(color.red("[ERR] 创建项目失败，未配置exec执行函数"));
+                    }
+                });
+            } else {
+                console.log(color.red("[Exit] 取消操作!"));
             }
-            inputProjectName((name) => {
-                const exec = project.exec;
-                delete project.exec;
-                project.name = name;
-                if(typeof exec === "function") {
-                    exec(project);
-                } else {
-                    console.log(color.red("[ERR] 创建项目失败，未配置exec执行函数"));
-                }
-            });
         });
     });
 
