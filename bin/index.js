ccc1e410e98a9b6c6d89ec7b62dd822586a69f1f
莫书江 <moshujiang@moshujiangdeMacBook-Pro.local>
2020-01-17 18:53:48 +0800

    update ignore file content

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 const program = require("./init");
-const translate = require("./translate");
+// const translate = require("./translate");
 
 program.command('help')
     .description('提示信息： 如何使用这个小工具.')
     .action(() => { program.outputHelp(); });
 
-translate(); // 初始化百度翻译的命令
+// translate(); // 初始化百度翻译的命令
 
 program.parse(process.argv);
