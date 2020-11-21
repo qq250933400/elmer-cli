14e90f689f8d739ad41df82d7ceaf4fa5752a599
莫书江 <moshujiang@moshujiangdeMacBook-Pro.local>
2020-11-21 09:58:22 +0800

    fix defect: can not bind 0.0.0.0 to the server

--- src/webpack/server.ts
+++ src/webpack/server.ts
@@ -1,6 +1,7 @@
 // import webpackDevServer from "webpack-dev-server";
 import "colors";
 import getConfig, { portIsOccupied, mergeUserConfig, TypeOverrideConfig } from "./config";
+import { StaticCommon as utils } from "elmer-common";
 const merge = require('webpack-merge');
 const webpack = require("webpack");
 const webpackDevServer = require("webpack-dev-server");
@@ -21,9 +22,10 @@ export default () => {
             webpackDevServer.addDevServerEntrypoints(configuration, options);
             const compiler = webpack(configuration);
             const server = new webpackDevServer(compiler, options);
-            server.listen(options.port, 'localhost', () => {
+            const host = !utils.isEmpty(options.host) ? options.host : "0.0.0.0";
+            server.listen(options.port, host, () => {
                 console.log("");
-                console.log(`block dev-server listening on port ${options.port}`.blue);
+                console.log(`block dev-server listening on  ${host}:${options.port}`.red);
                 console.log("");
             });
         } else {
